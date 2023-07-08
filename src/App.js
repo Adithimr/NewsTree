b266ff85d7d8ba95e568d496d38d0475e58ac823
Adithimr <104607743+Adithimr@users.noreply.github.com>
2023-07-08 11:28:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 import LoadingBar from 'react-top-loading-bar'
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 const App=()=>{
   const pageSize = 15;
@@ -14,26 +13,20 @@ const [progress,setProgress]=useState(0)
     return (
      
       <div>
-        <Router>
         <NavBar/>
         <LoadingBar
         color='#f11946'
         progress={progress}
         onLoaderFinished={() => setProgress(100)}
         />
-        
-        <Routes>
-          <Route path="/" element={<News setProgress ={setProgress} apiKey={apiKey}  key="general" pageSize={pageSize} country = "in" category = "general"/>}></Route>
-          <Route path="/business" element={<News setProgress ={setProgress} apiKey={apiKey}  key="business" pageSize={pageSize} country = "in" category = "business"/>}></Route>
-          <Route path="/entertainment" element={<News setProgress ={setProgress} apiKey={apiKey}  key="entertainment"  pageSize={pageSize} country = "in" category = "entertainment"/>}></Route>
-          <Route path="/general" element={<News setProgress ={setProgress} apiKey={apiKey}  key="general" pageSize={pageSize} country = "in" category = "general"/>}></Route>
-          <Route path="/health" element={<News setProgress ={setProgress} apiKey={apiKey}  key="health" pageSize={pageSize} country = "in" category = "health"/>}></Route>
-          <Route path="/science" element={<News setProgress ={setProgress} apiKey={apiKey}  key="science" pageSize={pageSize} country = "in" category = "science"/>}></Route>
-          <Route path="/sports" element={<News setProgress ={setProgress} apiKey={apiKey}  key="sports" pageSize={pageSize} country = "in" category = "sports"/>}></Route>
-          <Route path="/technology" element={<News setProgress ={setProgress} apiKey={apiKey}  key="technology" pageSize={pageSize} country = "in" category = "technology"/>}></Route>
-          </Routes>
-        </Router>
-        
+        <News setProgress ={setProgress} apiKey={apiKey}  key="general" pageSize={pageSize} country = "in" category = "general"/>
+         <News setProgress ={setProgress} apiKey={apiKey}  key="business" pageSize={pageSize} country = "in" category = "business"/>
+          <News setProgress ={setProgress} apiKey={apiKey}  key="entertainment"  pageSize={pageSize} country = "in" category = "entertainment"/>
+         <News setProgress ={setProgress} apiKey={apiKey}  key="general" pageSize={pageSize} country = "in" category = "general"/>
+       <News setProgress ={setProgress} apiKey={apiKey}  key="health" pageSize={pageSize} country = "in" category = "health"/>
+          <News setProgress ={setProgress} apiKey={apiKey}  key="science" pageSize={pageSize} country = "in" category = "science"/>
+        <News setProgress ={setProgress} apiKey={apiKey}  key="sports" pageSize={pageSize} country = "in" category = "sports"/>
+         <News setProgress ={setProgress} apiKey={apiKey}  key="technology" pageSize={pageSize} country = "in" category = "technology"/>
       </div>
     )
 }
